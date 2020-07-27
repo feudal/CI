5d68a78f994f89604918a581a8fbbecb625bf7f9
feudal <calmis87@gmail.com>
2020-07-27 12:05:50 +0300

    CodeIgniter Basics

---
+++ application/views/user_view.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>User View</title>
+</head>
+<body>
+
+
+<?php
+
+		foreach ($result as $object) {
+			echo $object->username.'<br>';
+		}
+
+?>
+
+</body>
+</html>
